7ab7b9c8cdadf933c8c7354279a3ed9fecc19cc1
Earu <ealrann@gmail.com>
2018-06-28 15:38:35 +0800

    Fix bad dataDepth for computation

--- org.sheepy.lily.game.vulkan/src/org/sheepy/lily/game/vulkan/pipeline/compute/ComputerPool.java
+++ org.sheepy.lily.game.vulkan/src/org/sheepy/lily/game/vulkan/pipeline/compute/ComputerPool.java
@@ -66,7 +66,7 @@ public class ComputerPool implements IDescriptorSetConfiguration
 
 	public int getDataDepth()
 	{
-		return dataHeight;
+		return dataDepth;
 	}
 
 	public List<IComputer> getComputers()
