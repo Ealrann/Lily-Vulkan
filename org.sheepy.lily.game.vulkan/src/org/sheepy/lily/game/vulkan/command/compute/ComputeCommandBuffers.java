cfe2e4f7e6ef25ffe2c0b2dd5c4e64ca2ea530bf
Earu <ealrann@gmail.com>
2018-07-06 16:28:46 +0800

    Use ComputeProcess to record the command

--- org.sheepy.lily.game.vulkan/src/org/sheepy/lily/game/vulkan/command/compute/ComputeCommandBuffers.java
+++ org.sheepy.lily.game.vulkan/src/org/sheepy/lily/game/vulkan/command/compute/ComputeCommandBuffers.java
@@ -1,28 +1,17 @@
 package org.sheepy.lily.game.vulkan.command.compute;
 
-import static org.lwjgl.vulkan.VK10.*;
-
-import java.nio.LongBuffer;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.joml.Vector3i;
-import org.lwjgl.system.MemoryUtil;
 import org.sheepy.lily.game.vulkan.command.AbstractCommandBuffers;
 import org.sheepy.lily.game.vulkan.command.CommandPool;
-import org.sheepy.lily.game.vulkan.descriptor.DescriptorPool;
-import org.sheepy.lily.game.vulkan.descriptor.DescriptorSet;
-import org.sheepy.lily.game.vulkan.pipeline.compute.ComputePipeline;
 import org.sheepy.lily.game.vulkan.pipeline.compute.ComputeProcess;
 import org.sheepy.lily.game.vulkan.pipeline.compute.ComputeProcessPool;
 
 public class ComputeCommandBuffers extends AbstractCommandBuffers<ComputeCommandBuffer>
 {
-	private static final float WORKGROUP_SIZE = 32f;
-
 	private ComputeProcessPool computeProcesses;
 
 	private Map<ComputeProcess, ComputeCommandBuffer> mapBuffers = new HashMap<>();
@@ -32,11 +21,10 @@ public class ComputeCommandBuffers extends AbstractCommandBuffers<ComputeCommand
 		super(commandPool);
 		this.computeProcesses = computeProcesses;
 	}
-
+	
 	@Override
 	protected List<ComputeCommandBuffer> allocCommandBuffers()
 	{
-
 		List<ComputeCommandBuffer> res = new ArrayList<>();
 		long commandPoolId = commandPool.getId();
 
@@ -45,55 +33,20 @@ public class ComputeCommandBuffers extends AbstractCommandBuffers<ComputeCommand
 
 		for (int i = 0; i < computeProcesses.getProcesses().size(); i++)
 		{
-			ComputeProcess computeprocess = computeProcesses.getProcesses().get(i);
+			ComputeProcess computeProcess = computeProcesses.getProcesses().get(i);
 			long commandBufferId = commandBufferIds[i];
-
-			DescriptorPool descriptorPool = computeprocess.getDescriptorPool();
-			Collection<ComputePipeline> pipelines = computeprocess.getPipelines();
 
 			ComputeCommandBuffer commandBuffer = new ComputeCommandBuffer(
 					commandPool.getLogicalDevice(), commandBufferId);
 
 			commandBuffer.start();
 
-			for (ComputePipeline pipeline : pipelines)
-			{
-				long pipelineLayout = pipeline.getPipelineLayout();
-
-				int dataWidth = pipeline.getDataWidth();
-				int dataHeight = pipeline.getDataHeight();
-				int dataDepth = pipeline.getDataDepth();
-
-				Vector3i groupCount = new Vector3i((int) Math.ceil(dataWidth / WORKGROUP_SIZE),
-						(int) Math.ceil(dataHeight / WORKGROUP_SIZE),
-						(int) Math.ceil(dataDepth / WORKGROUP_SIZE));
-
-				if (descriptorPool != null)
-				{
-					DescriptorSet descriptorSet = descriptorPool.getDescriptorSet(pipeline);
-
-					LongBuffer bDescriptorSet = MemoryUtil.memAllocLong(1);
-					bDescriptorSet.put(descriptorSet.getId());
-					bDescriptorSet.flip();
-
-					vkCmdBindDescriptorSets(commandBuffer.getVkCommandBuffer(),
-							VK_PIPELINE_BIND_POINT_COMPUTE, pipelineLayout, 0, bDescriptorSet,
-							null);
-
-					MemoryUtil.memFree(bDescriptorSet);
-				}
-
-				vkCmdBindPipeline(commandBuffer.getVkCommandBuffer(),
-						VK_PIPELINE_BIND_POINT_COMPUTE, pipeline.getId());
-
-				vkCmdDispatch(commandBuffer.getVkCommandBuffer(), groupCount.x, groupCount.y,
-						groupCount.z);
-			}
+			computeProcess.recordCommand(commandBuffer);
 
 			commandBuffer.end();
 
 			res.add(commandBuffer);
-			mapBuffers.put(computeprocess, commandBuffer);
+			mapBuffers.put(computeProcess, commandBuffer);
 		}
 
 		return res;
--- org.sheepy.lily.game.vulkan/src/org/sheepy/lily/game/vulkan/pipeline/compute/ComputeProcess.java
+++ org.sheepy.lily.game.vulkan/src/org/sheepy/lily/game/vulkan/pipeline/compute/ComputeProcess.java
@@ -1,11 +1,18 @@
 package org.sheepy.lily.game.vulkan.pipeline.compute;
 
+import static org.lwjgl.vulkan.VK10.*;
+
+import java.nio.LongBuffer;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.joml.Vector3i;
 import org.lwjgl.system.MemoryStack;
+import org.lwjgl.system.MemoryUtil;
+import org.sheepy.lily.game.vulkan.command.compute.ComputeCommandBuffer;
 import org.sheepy.lily.game.vulkan.descriptor.DescriptorPool;
+import org.sheepy.lily.game.vulkan.descriptor.DescriptorSet;
 import org.sheepy.lily.game.vulkan.device.LogicalDevice;
 
 /**
@@ -16,12 +23,16 @@ import org.sheepy.lily.game.vulkan.device.LogicalDevice;
  */
 public class ComputeProcess
 {
+	private static final float WORKGROUP_SIZE = 32f;
+
+	private LogicalDevice logicalDevice;
 	private List<ComputePipeline> computePipelines = new ArrayList<>();
 
 	protected DescriptorPool descriptorPool;
 
 	public ComputeProcess(LogicalDevice logicalDevice, List<ComputePipeline> computePipeline)
 	{
+		this.logicalDevice = logicalDevice;
 		this.computePipelines.addAll(computePipeline);
 
 		descriptorPool = new DescriptorPool(logicalDevice);
@@ -35,6 +46,11 @@ public class ComputeProcess
 	public void addPipeline(ComputePipeline pipeline)
 	{
 		computePipelines.add(pipeline);
+	}
+
+	public void addNewPipeline(IComputer computer)
+	{
+		addPipeline(new ComputePipeline(logicalDevice, computer));
 	}
 
 	public void load(MemoryStack stack)
@@ -54,6 +70,42 @@ public class ComputeProcess
 		for (ComputePipeline pipeline : computePipelines)
 		{
 			pipeline.free();
+		}
+	}
+
+	public void recordCommand(ComputeCommandBuffer commandBuffer)
+	{
+		for (ComputePipeline pipeline : computePipelines)
+		{
+			long pipelineLayout = pipeline.getPipelineLayout();
+
+			int dataWidth = pipeline.getDataWidth();
+			int dataHeight = pipeline.getDataHeight();
+			int dataDepth = pipeline.getDataDepth();
+
+			Vector3i groupCount = new Vector3i((int) Math.ceil(dataWidth / WORKGROUP_SIZE),
+					(int) Math.ceil(dataHeight / WORKGROUP_SIZE),
+					(int) Math.ceil(dataDepth / WORKGROUP_SIZE));
+
+			if (descriptorPool != null)
+			{
+				DescriptorSet descriptorSet = descriptorPool.getDescriptorSet(pipeline);
+
+				LongBuffer bDescriptorSet = MemoryUtil.memAllocLong(1);
+				bDescriptorSet.put(descriptorSet.getId());
+				bDescriptorSet.flip();
+
+				vkCmdBindDescriptorSets(commandBuffer.getVkCommandBuffer(),
+						VK_PIPELINE_BIND_POINT_COMPUTE, pipelineLayout, 0, bDescriptorSet, null);
+
+				MemoryUtil.memFree(bDescriptorSet);
+			}
+
+			vkCmdBindPipeline(commandBuffer.getVkCommandBuffer(), VK_PIPELINE_BIND_POINT_COMPUTE,
+					pipeline.getId());
+
+			vkCmdDispatch(commandBuffer.getVkCommandBuffer(), groupCount.x, groupCount.y,
+					groupCount.z);
 		}
 	}
 
