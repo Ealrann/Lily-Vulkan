d3705d2e1c6a31cd036b390862e3928b2eec3418
Earu <ealrann@gmail.com>
2018-07-16 18:20:43 +0800

    Rework the file to buffer util. The shaders can now load correctly under a jar
    Former-commit-id: dd0187b54f0f45337f2d25c38d05ec4a030a00d5

--- org.sheepy.vulkan/src/org/sheepy/vulkan/util/VulkanBufferUtils.java
+++ org.sheepy.vulkan/src/org/sheepy/vulkan/util/VulkanBufferUtils.java
@@ -1,20 +1,11 @@
 package org.sheepy.vulkan.util;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 import java.nio.ByteBuffer;
 import java.nio.LongBuffer;
-import java.nio.channels.Channels;
-import java.nio.channels.FileChannel;
-import java.nio.channels.ReadableByteChannel;
 import java.util.ArrayList;
 import java.util.List;
-
-import org.lwjgl.BufferUtils;
 
 public class VulkanBufferUtils
 {
@@ -34,59 +25,30 @@ public class VulkanBufferUtils
 	public static ByteBuffer ioResourceToByteBuffer(String resource, int initialBufferSize)
 			throws IOException
 	{
-		ByteBuffer buffer;
-		URL url = Thread.currentThread().getContextClassLoader().getResource(resource);
-		File file = new File(url.getFile());
-		if (file.isFile())
+		System.out.println(resource);
+		ByteBuffer buffer = null;
+		InputStream fis = null;
+
+		try
 		{
-			FileInputStream fis = new FileInputStream(file);
-			FileChannel fc = fis.getChannel();
-			buffer = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
-			fc.close();
-			fis.close();
-		}
-		else
+			fis = Thread.currentThread().getContextClassLoader().getResourceAsStream(resource);
+			byte[] byteArray = fis.readAllBytes();
+
+			buffer = ByteBuffer.allocateDirect(byteArray.length);
+
+			buffer.put(byteArray);
+			buffer.flip();
+
+		} catch (Exception e)
 		{
-			buffer = BufferUtils.createByteBuffer(initialBufferSize);
-			InputStream source = url.openStream();
-			if (source == null)
-				throw new FileNotFoundException(resource);
-			try
-			{
-				ReadableByteChannel rbc = Channels.newChannel(source);
-				try
-				{
-					while (true)
-					{
-						int bytes = rbc.read(buffer);
-						if (bytes == -1)
-							break;
-						if (buffer.remaining() == 0)
-							buffer = resizeBuffer(buffer, buffer.capacity() * 2);
-					}
-					buffer.flip();
-				} finally
-				{
-					rbc.close();
-				}
-			} finally
-			{
-				source.close();
-			}
+			e.printStackTrace();
+		} finally
+		{
+			if (fis != null) fis.close();
 		}
 		return buffer;
 	}
 
-	private static ByteBuffer resizeBuffer(ByteBuffer buffer, int newCapacity)
-	{
-		ByteBuffer newBuffer = BufferUtils.createByteBuffer(newCapacity);
-		buffer.flip();
-		newBuffer.put(buffer);
-		newBuffer.flip();
-		return newBuffer;
-	}
-
-	
 	public static final long[] toArray(LongBuffer buffer)
 	{
 		long[] res = new long[buffer.capacity()];
@@ -99,7 +61,6 @@ public class VulkanBufferUtils
 		return res;
 	}
 
-	
 	public static final List<Long> toList(LongBuffer buffer)
 	{
 		List<Long> res = new ArrayList<>();
