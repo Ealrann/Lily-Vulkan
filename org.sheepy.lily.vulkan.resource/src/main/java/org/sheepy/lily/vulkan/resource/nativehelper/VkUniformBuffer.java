ca2b4f102a916b1e716928baf3d8fc1cd2dab7f6
Aurélien Mora <ealrann@gmail.com>
2019-04-03 23:10:06 +0800

    New helper class: VkUniformBuffer

---
+++ org.sheepy.lily.vulkan.resource/src/main/java/org/sheepy/lily/vulkan/resource/nativehelper/VkUniformBuffer.java
@@ -0,0 +1,106 @@
+package org.sheepy.lily.vulkan.resource.nativehelper;
+
+import static org.lwjgl.vulkan.VK10.VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+
+import java.util.List;
+
+import org.lwjgl.system.MemoryStack;
+import org.lwjgl.vulkan.VkDescriptorBufferInfo;
+import org.lwjgl.vulkan.VkDescriptorPoolSize;
+import org.lwjgl.vulkan.VkDescriptorSetLayoutBinding;
+import org.lwjgl.vulkan.VkWriteDescriptorSet;
+import org.sheepy.lily.vulkan.api.allocation.IAllocable;
+import org.sheepy.lily.vulkan.api.allocation.IAllocationContext;
+import org.sheepy.lily.vulkan.api.resource.IVkDescriptor;
+import org.sheepy.lily.vulkan.common.util.ModelUtil;
+import org.sheepy.lily.vulkan.model.enumeration.EDescriptorType;
+import org.sheepy.lily.vulkan.model.enumeration.EShaderStage;
+import org.sheepy.lily.vulkan.resource.buffer.BufferInfo;
+import org.sheepy.lily.vulkan.resource.buffer.GPUBufferBackend;
+
+public class VkUniformBuffer implements IVkDescriptor, IAllocable
+{
+	private final GPUBufferBackend uniformBuffer;
+	private final List<EShaderStage> stages;
+
+	public VkUniformBuffer(BufferInfo info, List<EShaderStage> stages)
+	{
+		this.stages = stages;
+		uniformBuffer = new GPUBufferBackend(info, false);
+	}
+
+	@Override
+	public void allocate(MemoryStack stack, IAllocationContext context)
+	{
+		uniformBuffer.allocate(stack, context);
+	}
+
+	@Override
+	public void free(IAllocationContext context)
+	{
+		uniformBuffer.free(context);
+	}
+
+	@Override
+	public boolean isAllocationDirty(IAllocationContext context)
+	{
+		return uniformBuffer.isAllocationDirty(context);
+	}
+
+	@Override
+	public VkDescriptorPoolSize allocPoolSize(MemoryStack stack)
+	{
+		final VkDescriptorPoolSize poolSize = VkDescriptorPoolSize.callocStack(stack);
+		poolSize.type(EDescriptorType.UNIFORM_BUFFER_VALUE);
+		poolSize.descriptorCount(getDescriptorCount());
+		return poolSize;
+	}
+
+	@Override
+	public VkDescriptorSetLayoutBinding allocLayoutBinding(MemoryStack stack)
+	{
+		final int stageFlags = ModelUtil.getEnumeratedFlag(stages);
+		final VkDescriptorSetLayoutBinding res = VkDescriptorSetLayoutBinding.callocStack(stack);
+		res.descriptorType(EDescriptorType.UNIFORM_BUFFER_VALUE);
+		res.descriptorCount(getDescriptorCount());
+		res.stageFlags(stageFlags);
+		return res;
+	}
+
+	@Override
+	public VkWriteDescriptorSet allocWriteDescriptor(MemoryStack stack)
+	{
+		final VkDescriptorBufferInfo.Buffer bufferInfo = allocBufferInfo(stack);
+
+		final VkWriteDescriptorSet descriptorWrite = VkWriteDescriptorSet.callocStack(stack);
+		descriptorWrite.sType(VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET);
+		descriptorWrite.dstArrayElement(0);
+		descriptorWrite.descriptorType(EDescriptorType.UNIFORM_BUFFER_VALUE);
+		descriptorWrite.pBufferInfo(bufferInfo);
+		descriptorWrite.pImageInfo(null); // Optional
+		descriptorWrite.pTexelBufferView(null); // Optional
+		return descriptorWrite;
+	}
+
+	protected VkDescriptorBufferInfo.Buffer allocBufferInfo(MemoryStack stack)
+	{
+		final VkDescriptorBufferInfo.Buffer bufferInfo = VkDescriptorBufferInfo.callocStack(1,
+				stack);
+		bufferInfo.buffer(uniformBuffer.getAddress());
+		bufferInfo.offset(0);
+		bufferInfo.range(uniformBuffer.getInfos().size);
+
+		return bufferInfo;
+	}
+
+	protected static int getDescriptorCount()
+	{
+		return 1;
+	}
+
+	public GPUBufferBackend getBuffer()
+	{
+		return uniformBuffer;
+	}
+
+}
