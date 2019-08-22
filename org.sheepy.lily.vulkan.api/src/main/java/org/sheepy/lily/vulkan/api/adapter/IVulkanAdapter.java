a54a14117bbd7000d420808768bec55238624a7d
Aurélien Mora <ealrann@gmail.com>
2019-08-22 20:42:39 +0800

    Useless code

--- org.sheepy.lily.vulkan.api/src/main/java/org/sheepy/lily/vulkan/api/adapter/IVulkanAdapter.java
+++ org.sheepy.lily.vulkan.api/src/main/java/org/sheepy/lily/vulkan/api/adapter/IVulkanAdapter.java
@@ -1,14 +1,6 @@
 package org.sheepy.lily.vulkan.api.adapter;
 
-import java.util.Iterator;
-import java.util.ServiceLoader;
-
 import org.sheepy.lily.core.api.adapter.IAdapter;
 
 public interface IVulkanAdapter extends IAdapter
-{
-	static Iterator<IVulkanAdapter> getServices()
-	{
-		return ServiceLoader.load(IVulkanAdapter.class).iterator();
-	}
-}
+{}
