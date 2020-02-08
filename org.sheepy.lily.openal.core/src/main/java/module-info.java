f2af23e86ae1d355173000abbe5d254653cf90d3
Aurélien Mora <ealrann@gmail.com>
2020-02-08 16:35:47 +0100

    Fix openAl on windows

--- org.sheepy.lily.openal.core/src/main/java/module-info.java
+++ org.sheepy.lily.openal.core/src/main/java/module-info.java
@@ -7,6 +7,7 @@ module org.sheepy.lily.openal.core
 	requires org.sheepy.lily.game.core;
 
 	requires org.lwjgl.stb;
+	requires org.lwjgl.openal.natives;
 
 	opens org.sheepy.lily.openal.core.engine;
 	opens org.sheepy.lily.openal.core.resource;
