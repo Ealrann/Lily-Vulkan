901c3ca2a1a7aebe75fbe97816558db543d3c53b
Aurélien Mora <ealrann@gmail.com>
2020-03-13 18:58:52 +0100

    Fix bad parameter

--- org.sheepy.lily.vulkan.process/src/main/java/org/sheepy/lily/vulkan/process/barrier/ImageBarrierAdapter.java
+++ org.sheepy.lily.vulkan.process/src/main/java/org/sheepy/lily/vulkan/process/barrier/ImageBarrierAdapter.java
@@ -8,13 +8,11 @@ import org.sheepy.lily.vulkan.core.resource.IVkImageAdapter;
 import org.sheepy.lily.vulkan.model.resource.ImageBarrier;
 import org.sheepy.lily.vulkan.model.resource.VulkanResourcePackage;
 
-import java.util.function.LongConsumer;
-
 @Statefull
 @Adapter(scope = ImageBarrier.class)
 public class ImageBarrierAdapter implements IImageBarrierAdapter
 {
-	private final LongConsumer imageListener = this::imageChanged;
+	private final Runnable imageListener = this::imageChanged;
 	private final ImageBarrier imageBarrier;
 
 	private VkImageBarrier vkBarrier;
@@ -26,7 +24,7 @@ public class ImageBarrierAdapter implements IImageBarrierAdapter
 	}
 
 	@NotifyChanged(featureIds = VulkanResourcePackage.IMAGE_BARRIER__IMAGE)
-	private void imageChanged(Long ptr)
+	private void imageChanged()
 	{
 		changed = true;
 	}
@@ -35,7 +33,7 @@ public class ImageBarrierAdapter implements IImageBarrierAdapter
 	private void load()
 	{
 		final var adapter = imageBarrier.getImage().adapt(IVkImageAdapter.class);
-		adapter.listen(imageListener, IVkImageAdapter.Features.Image);
+		adapter.listenNoParam(imageListener, IVkImageAdapter.Features.Image);
 
 		final var image = imageBarrier.getImage();
 		final var imageAdapter = image.adapt(IVkImageAdapter.class);
@@ -59,7 +57,7 @@ public class ImageBarrierAdapter implements IImageBarrierAdapter
 	private void dispose()
 	{
 		final var adapter = imageBarrier.getImage().adapt(IVkImageAdapter.class);
-		adapter.sulk(imageListener, IVkImageAdapter.Features.Image);
+		adapter.sulkNoParam(imageListener, IVkImageAdapter.Features.Image);
 	}
 
 	@Override
