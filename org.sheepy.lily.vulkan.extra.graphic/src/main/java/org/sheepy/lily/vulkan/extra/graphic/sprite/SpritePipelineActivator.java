0323daea6f3e917e86afad7b278b411a198ce657
Aurélien Mora <ealrann@gmail.com>
2020-10-14 22:08:23 +0200

    Don't allocate SpritePipeline when no spriteImages

--- org.sheepy.lily.vulkan.extra.graphic/src/main/java/org/sheepy/lily/vulkan/extra/graphic/sprite/SpritePipelineActivator.java
+++ org.sheepy.lily.vulkan.extra.graphic/src/main/java/org/sheepy/lily/vulkan/extra/graphic/sprite/SpritePipelineActivator.java
@@ -4,6 +4,7 @@ import org.sheepy.lily.core.api.adapter.annotation.Adapter;
 import org.sheepy.lily.core.api.extender.IExtender;
 import org.sheepy.lily.core.api.extender.ModelExtender;
 import org.sheepy.lily.core.api.notification.observatory.IObservatoryBuilder;
+import org.sheepy.lily.vulkan.extra.api.sprite.ISpriteSupplier;
 import org.sheepy.lily.vulkan.extra.api.sprite.ISpriteVertexSupplier;
 import org.sheepy.lily.vulkan.extra.model.sprite.SpriteExtension;
 import org.sheepy.lily.vulkan.model.process.graphic.GraphicsPipeline;
@@ -16,18 +17,29 @@ public final class SpritePipelineActivator implements IExtender
 {
 	private SpritePipelineActivator(final SpriteExtension spriteExtension, final IObservatoryBuilder observatory)
 	{
-		final var spriteVertexSupplier = spriteExtension.getSpriteDataSource()
-														.adaptNotNull(ISpriteVertexSupplier.class);
+		final var dataSource = spriteExtension.getSpriteDataSource();
+		final var spriteSupplier = dataSource.adaptNotNull(ISpriteSupplier.class);
+		final var spriteVertexSupplier = dataSource.adaptNotNull(ISpriteVertexSupplier.class);
 		final var spritePipeline = (GraphicsPipeline) spriteExtension.eContainer().eContainer();
-		final IntConsumer pipelineActivator = count -> activate(spritePipeline, count);
+		final IntConsumer recordActivator = entityCount -> activateRecord(spritePipeline, entityCount);
+		final Runnable allocationActivator = () -> activateAllocation(spritePipeline, spriteSupplier.imageCount());
 
-		pipelineActivator.accept(spriteVertexSupplier.entityCount());
-		observatory.focus(spriteVertexSupplier).listen(pipelineActivator, ISpriteVertexSupplier.Features.EntityCount);
+		recordActivator.accept(spriteVertexSupplier.entityCount());
+		observatory.focus(spriteVertexSupplier).listen(recordActivator, ISpriteVertexSupplier.Features.EntityCount);
+
+		allocationActivator.run();
+		observatory.focus(spriteSupplier).listenNoParam(allocationActivator, ISpriteSupplier.Features.Sprites);
 	}
 
-	private static void activate(final GraphicsPipeline spritePipeline, final int count)
+	private static void activateRecord(final GraphicsPipeline spritePipeline, final int entityCount)
 	{
-		final var activation = count != 0;
+		final var activation = entityCount != 0;
 		spritePipeline.setRecord(activation);
+	}
+
+	private static void activateAllocation(final GraphicsPipeline spritePipeline, final int spriteCount)
+	{
+		final var activation = spriteCount != 0;
+		spritePipeline.setAllocate(activation);
 	}
 }
