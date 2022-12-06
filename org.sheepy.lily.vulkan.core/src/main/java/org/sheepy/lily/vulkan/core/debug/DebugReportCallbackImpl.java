3d72976ad183868aaa19cba04aa8ff2dc5ef85b5
Aurélien Mora <ealrann@gmail.com>
2022-12-06 19:59:47 +0100

    Fix big 64bit hex to pointers

--- org.sheepy.lily.vulkan.core/src/main/java/org/sheepy/lily/vulkan/core/debug/DebugReportCallbackImpl.java
+++ org.sheepy.lily.vulkan.core/src/main/java/org/sheepy/lily/vulkan/core/debug/DebugReportCallbackImpl.java
@@ -3,11 +3,13 @@ package org.sheepy.lily.vulkan.core.debug;
 import org.lwjgl.vulkan.VkDebugReportCallbackEXT;
 import org.sheepy.lily.vulkan.api.debug.IVulkanDebugService;
 
+import java.math.BigInteger;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 
 public final class DebugReportCallbackImpl extends VkDebugReportCallbackEXT
 {
+	public static final BigInteger NEG = BigInteger.ONE.shiftLeft(127);
 	private static final Pattern PATTERN = Pattern.compile("0x[0-9a-fA-F]+");
 	private static final IVulkanDebugService debugService = IVulkanDebugService.INSTANCE;
 
@@ -32,7 +34,13 @@ public final class DebugReportCallbackImpl extends VkDebugReportCallbackEXT
 	private static String buildFriendlyName(MatchResult result)
 	{
 		final var hexString = result.group();
-		final var ptr = Long.decode(hexString);
+
+		BigInteger b = new BigInteger(hexString.substring(2), 16);
+		if(b.compareTo(NEG) >= 0) {
+			b = NEG.subtract(b);
+		}
+
+		final var ptr = b.longValue();
 		final var name = debugService.get(ptr);
 		return name != null ? name + " (" + hexString + ")" : hexString;
 	}
